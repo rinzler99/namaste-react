0d585a834b523b91f6c95bddf17171e140b3db63
Pradeep <pradeep@tor.us>
2023-02-07 16:38:09 +0530

    no create-react-app

---
+++ app.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client'
+
+const heading = React.createElement(
+    "h1", 
+    { id: "heading"}, 
+    "Welcome to Namaste react"
+); 
+
+const heading1 = React.createElement(
+    "h2", 
+    { id: "heading1"}, 
+    "This is OG React!"
+); 
+
+const container = React.createElement(
+    "div",
+    { id: "container"},
+    [heading, heading1]
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(container);
